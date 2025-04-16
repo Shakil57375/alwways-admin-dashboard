a4bdb373543f1e429eaa44b4dfc85685ad078413
Shakil57375 <shakil57375@gmail.com>
2025-04-16 08:25:07 +0600

    updated

--- src/pages/Dashboard/TermsAndCondition.jsx
+++ src/pages/Dashboard/TermsAndCondition.jsx
@@ -5,7 +5,7 @@ import RichTextEditor from "../../components/RichTextEditor/RichTextEditor"
 const TermsAndCondition = () => {
   return (
     <div className="p-6">
-      <p className="text-3xl font-bold mb-8 text-black">Terms and Conditions</p>
+      <p className="text-3xl font-bold mb-8 text-black ">Terms and Conditions</p>
       <RichTextEditor type="terms" />
     </div>
   )
